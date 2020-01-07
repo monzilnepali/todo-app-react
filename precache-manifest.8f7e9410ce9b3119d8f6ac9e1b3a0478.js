5ab92c8c4a68ba2adc532a353c3b8555070f2aa1
manjilnepali <manjilnepali98@gmail.com>
2020-01-07 18:20:49 +0545

    Updates

---
+++ precache-manifest.8f7e9410ce9b3119d8f6ac9e1b3a0478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479fbf6ddddfa50d8627cd644c923d6e",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "c184ddbb859b6ccfd87d",
+    "url": "/todo-app-react/static/css/main.a3179e58.chunk.css"
+  },
+  {
+    "revision": "b09902408e5cbe462e00",
+    "url": "/todo-app-react/static/js/2.ff8892fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app-react/static/js/2.ff8892fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c184ddbb859b6ccfd87d",
+    "url": "/todo-app-react/static/js/main.6ceb67bd.chunk.js"
+  },
+  {
+    "revision": "c6196d61ed91e2c54783",
+    "url": "/todo-app-react/static/js/runtime-main.b72d4870.js"
+  }
+]);
