e066cc90cf3f8bd14127dda5c0993acb02cec696
monzilnepali <manjilnepali98@gmail.com>
2020-09-19 17:59:04 +0545

    Firebase Setup and firestore indexes config added

---
+++ build/precache-manifest.de4e70e7ea66db93ddad2967f44f1367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a1f588ef555f2599c514d1a874353e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25cb407afcc871f5eb85",
+    "url": "/static/css/main.efc0d5ce.chunk.css"
+  },
+  {
+    "revision": "36122da8726847d593ea",
+    "url": "/static/js/2.ffb14212.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ffb14212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25cb407afcc871f5eb85",
+    "url": "/static/js/main.5902ad69.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
--- src/Components/App.js
+++ src/Components/App.js
@@ -89,6 +89,7 @@ class App extends React.Component{
             }
             return item;
         }
+        return null
 
     });
 
